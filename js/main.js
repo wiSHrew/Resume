88ac29b5e0edf296c44c1421bcf430e9558c778c
wiSHrew <76044597+wiSHrew@users.noreply.github.com>
2023-02-23 00:10:25 +0800

    back to layout1 bug fix

--- js/main.js
+++ js/main.js
@@ -14,16 +14,20 @@ $(document).ready(function(){
             $("#layout1").addClass("hide")
             $("#layout2").slideDown("slow")
             $("#layout2").removeClass("hide")
-            console.log(num, $("*").css("font-family"))
         }
         else{
             num = 0;
             alert("Thats all i have. still working on more:)")
+            $("*").css("font-family", "Calibri")
+            $("*").css("background-color", "#fcffe7")
+            $(".boxBottom").css("background-color", "#2b3467")
+            $("#endText").css("background-color", "#2b3467")
+            $("#endText").css("color", "#bad7e9")
+            $(".introduction").css("background-color", "#2b3467")
             $("#layout2").slideUp("slow")
             $("#layout2").addClass("hide")
             $("#layout1").slideDown("slow")
             $("#layout1").removeClass("hide")
-            console.log(num, $("*").css("font-family"))
         }
     })
 });
